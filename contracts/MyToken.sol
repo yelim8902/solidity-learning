e07ca0195e819be2fa06873273f08e0784e18eb2
yelim8902 <yelim2849@gmail.com>
2025-10-14 22:33:13 +0900

    add MyToken contract and compile artifacts

---
+++ contracts/MyToken.sol
@@ -0,0 +1,42 @@
+// Token : smart conntact based
+// BIT, ETH, XRP, KAIA : native token -> 해당 네트워크에서 수수료는 해당 토큰으로만 가능
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals; // uint8 -> 8bit unsigned int, unit16, ... , unit256
+    // 1 ETH = 10^18 wei, 1 wei = 10^-18 ETH
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimal){ // 문자열 앞에 memory 붙이는 이유 : 문자열은 메모리에 저장되어야 하기 때문
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimal;
+    }
+
+    // function totalSupply() external view returns (uint256) { //외부에서만 호출, 필드를 보기만 할거임
+    //     return totalSupply;
+    // }
+
+    // function balanceOf(address _owner) external view returns (uint256) {
+    //     return balanceOf[_owner];
+    // }
+
+    // function name() external view returns (string memory) {
+    //     return name;
+    // }
+
+    // function symbol() external view returns (string memory) {
+    //     return symbol;
+    // }
+
+    // function decimals() external view returns (uint8) {
+    //     return decimals;
+    // }
+}
