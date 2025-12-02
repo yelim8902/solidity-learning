491598a2e3486b86349205d5bee5c33c9086b821
yelim8902 <yelim2849@gmail.com>
2025-12-02 22:26:57 +0900

    add hardhat vyper plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-vyper";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
