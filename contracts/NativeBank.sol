e2d284dc96bfb382c29e056e70f97be1aef19a5f
yelim8902 <yelim2849@gmail.com>
2025-12-02 22:20:16 +0900

    add ReentrancyGuard protection to NativeBank

--- contracts/NativeBank.sol
+++ contracts/NativeBank.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract NativeBank {
+import "./ReentrancyGuard.sol";
+
+contract NativeBank is ReentrancyGuard {
     mapping(address => uint256) public balanceOf;
 
-    function withdraw() external {
+    function withdraw() external nonReentrant {
         uint256 balance = balanceOf[msg.sender];
         require(balance > 0, "insufficient balance");
 
+        balanceOf[msg.sender] = 0;
+
         (bool success, ) = msg.sender.call{value: balance}("");
         require(success, "failed to send native token");
-
-        balanceOf[msg.sender] = 0;
     }
 
     receive() external payable {
